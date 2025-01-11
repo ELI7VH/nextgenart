3a2011c4e64354c6ff297d2d87f811e87821b419
Elijah <elijahlucian@gmail.com>
2025-01-11 09:28:45 -0700

    gentoy-11-01-2025

--- sketchy-3d/src/main.ts
+++ sketchy-3d/src/main.ts
@@ -36,36 +36,41 @@ const sketch = create3dSketch(
       }
     })
 
-    // window.addEventListener('keypress', (e) => {
-    //   if (e.key === 's') {
-    //     // take screenshot
-
-    //     renderer.domElement.toBlob((blob) => {
-    //       const a = document.createElement('a')
-    //       a.href = URL.createObjectURL(blob)
-    //       a.download = 'screenshot.png'
-    //       a.click()
-    //     })
-
-    //     e.preventDefault()
-    //   }
-    // })
+    const debug = {
+      active: false,
+      element: document.getElementById('debug')!,
+      text: '',
+      reset() {
+        this.text = ''
+        this.element.textContent = ''
+      },
+      update(...text: (string | number)[]) {
+        if (!this.active) return
+        this.text += text.join(' ') + '\n'
+        this.element.textContent = this.text
+      },
+    }
 
     const ambient = useAmbient(0xffffff, 0.5)
     scene.add(ambient)
 
-    const light = useLight(0xffffff, 2, [1, 1, 1])
+    const light = useLight(0xffffff, 2, [1, -1, 0])
     scene.add(light)
 
     const data = {
       clicked: false,
       rotation: { x: PI * 0.75, y: 0, z: 0 },
+      xLim: 4,
+      yLim: 5,
+      index: 0,
+      jndex: 0,
       scroll: {
         x: 0,
         y: 0,
         z: 0,
+        dir: 1,
       },
-      scaleFactor: 1.7,
+      scaleFactor: 1,
       scale() {
         return [
           1 * this.scaleFactor,
@@ -83,7 +88,14 @@ const sketch = create3dSketch(
 
       data.scroll.y = e.deltaY
 
-      // box.rotation.y -= deltaY
+      const dir = e.deltaY > 0 ? 1 : -1
+
+      if (data.scroll.dir !== dir) {
+        data.jndex += 1
+        data.scroll.dir = dir
+      }
+
+      data.index += data.scroll.dir
     }
     mouse.onClick = (e) => {
       e.stopPropagation()
@@ -92,29 +104,32 @@ const sketch = create3dSketch(
 
       data.clicked = !data.clicked
       // box.rotation.x += PI * 0.25
-      data.rotation.x += PI * (e.button === 0 ? 0.25 : -0.25)
+      data.rotation.x += TAU * Rando.normal()
+      data.rotation.z += TAU * Rando.normal()
     }
 
-    const bounds: OrthographicCameraBounds = [-5, 5, 5, -5]
+    const xLim = data.xLim
+    const yLim = data.yLim
 
+    const bounds: OrthographicCameraBounds = [-xLim, xLim, -yLim, yLim]
     camera = useOrthographicCamera(bounds)
     const depth = 10
     camera.position.set(0, depth, 0)
     camera.lookAt(0, 0, 0)
     scene.add(camera)
 
-    const xLim = 20
-    const yLim = 20
     const cubes = mapXY(xLim, yLim, (u, v) => {
       const i = u + v
 
+      const h = Math.floor((Rando.normal() * 0.02 + 0.55) * 10) / 10
+
       const cube = useMesh(
-        useBox(data.scale()),
-        useStandardMaterial(hsl(Rando.normal(), 1, 0.8)),
+        useBox([1, 1, 1]),
+        useStandardMaterial(hsl(h, 1, 0.4)),
       )
 
       // this is wrong I think
-      const x = Maff.map(u, bounds[0] - 1, bounds[1] + 1)
+      const x = Maff.map(u, bounds[0] + 1, bounds[1] - 1)
       const y = Maff.map(i, 0, -depth * 10)
       const z = Maff.map(v, bounds[2] + 1, bounds[3] - 1)
 
@@ -127,25 +142,60 @@ const sketch = create3dSketch(
       cube.u = u
       cube.v = v
       cube.i = i
+      cube.bb = 0
+      cube.rand = Rando.normal()
+      cube.dir = Rando.normal() > 0.5 ? 1 : -1
+
+      cube.rotation.x = Rando.normal() * TAU
+      cube.rotation.y = Rando.normal() * TAU
+      cube.rotation.z = Rando.normal() * TAU
 
       cube.ui = i / (xLim * yLim)
 
       return cube
     })
 
-    return ({ time, dt }) => {
-      // box.position.y = 1 + Math.sin(time) * 1
+    const colors = [0xff69b4, 0x00ffff, 0x9370db] // hot pink, cyan, medium purple
 
-      cubes.forEach((cube) => {
-        cube.rotation.y = 0.5 * time + cube.i * 2
-        cube.rotation.x = data.rotation.x
-        cube.position.set(cube.x, cube.y, cube.z + sin(time + cube.i, 0.5, 0.1))
-        cube.material.opacity = sin(time + cube.i, 0.5, 0.1)
+    return ({ time, dt }) => {
+      debug.reset()
+      debug.update(container.clientWidth, container.clientHeight)
+
+      if (data.scroll.dir < 0) {
+        // scene bg to white
+        renderer.setClearColor(0xffffff, 1)
+      } else {
+        renderer.setClearColor(0x000000, 1)
+      }
+
+      cubes.forEach((cube, i) => {
+        if (!cube) return
+
+        cube.rotation.y =
+          (Math.floor(cube.rand * 2 * time + cube.i * 2 + cube.rand * 215 * 5) /
+            5) *
+          cube.dir
+        cube.rotation.x = Math.floor(data.rotation.x * 6) / 6
+        cube.position.set(cube.x, cube.y, cube.z)
+
+        const ai = Math.abs(data.index)
+        const ii = ai % cubes.length
+
+        if (ii === i) {
+          cube.rotation.y += mouse.scrollInertia * 0.05
+          debug.update(ai, ii, data.jndex)
+
+          const palleteIndex =
+            (Math.floor(ai / cubes.length) + data.jndex) % colors.length
+          debug.update(palleteIndex)
+
+          cube.material.color.set(colors[palleteIndex])
+        }
       })
 
       if (mouse.scrollInertia >= 0) {
         light.color.set(0xffffff)
-        light.intensity = 2 + mouse.scrollInertia * 0.01
+        // light.intensity = 2 + mouse.scrollInertia * 0.01
         ambient.intensity = 0 + mouse.scrollInertia * 0.0001
         // box.scale.set(
         //   1 + mouse.scrollInertia * 0.002,
